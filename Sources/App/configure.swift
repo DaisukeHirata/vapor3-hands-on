830fdec07a96b62d243d09da0e2aa89c1457f4bb
Daisuke Hirata <daisukihirata@gmail.com>
2018-06-06 09:52:47 +0900

    use Enviroment variable for database connection

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,11 +18,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Configure a PostgreSQL database
     var databases = DatabasesConfig()
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor",
-        password: "password")
+        hostname: hostname,
+        username: username,
+        database: databaseName,
+        password: password)
     let database = PostgreSQLDatabase(config: databaseConfig)
 
     /// Register the configured PostgreSQL database to the database config.
